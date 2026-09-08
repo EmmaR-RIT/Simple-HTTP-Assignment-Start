c2a97570b87bb079d3090c600133701e82b05d1f
Emma Rausch (RIT Student) <jsr4922@rit.edu>
2026-09-08 15:44:01 -0400

    finished assignment

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const textHandler = require('./textResponses.js');
 const jsonHandler = require('./jsonResponses.js');
+const imageHandler = require('./imageResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -27,6 +28,9 @@ const onRequest = (req, res) => {
             break;
         case '/timeJSON':
             jsonHandler.getTimeJSON(req, res);
+            break;
+        case '/dankmemes':
+            imageHandler.getImage(req, res);
             break;
         default:
             htmlHandler.getIndex(req, res);
